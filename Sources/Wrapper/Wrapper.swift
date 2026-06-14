e73cf684dcc54eb1ed3ce6e70327ac6aedc7c37e
finix-devops-sa <devops-git@finixpayments.com>
2026-06-14 03:50:00 +0000

    Update FinixPaymentSheet.xcframework at 9be1faed33e06e7b6e4f11cfcefd663734d2ad09

--- Sources/Wrapper/Wrapper.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-// This wrapper target makes the FinixPaymentSheetBinary XCFramework available as a Swift package
-// The XCFramework contains both PaymentSheet and CardScanner code compiled together as a standalone binary
